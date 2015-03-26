69cdc9ab3a7acd86ea7e5b67bdb77e3f248c2d79
Hoang Truong <quochoangddt@gmail.com>
2015-03-27 00:41:04 +0700

    Add return the name of objects in models

--- dashboard/models.py
+++ dashboard/models.py
@@ -5,8 +5,12 @@ from django.contrib.auth.models import User
 class Task(models.Model):
 	name = models.CharField(max_length=100)
 	description = models.TextField(null=True, blank = True)
+	def __str__(self):
+		return self.name
 
 
 class Supervisor(models.Model):
 	user = models.ForeignKey(User, null=True, related_name="supervisor")
 	task = models.ManyToManyField(Task, related_name="task")
+	def __str__(self):
+		return self.user.name
